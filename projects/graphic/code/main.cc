ebef3fb1500685a2c664db7f3151ecaee2eeb27e
dkingafg6 <61297626+dkingafg6@users.noreply.github.com>
2024-09-11 15:40:24 +0200

    final

---
+++ projects/graphic/code/main.cc
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------
+// main.cc
+// (C) 2015-2022 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "config.h"
+#include "exampleapp.h"
+//#include "vec4.h"
+//#include "vec3.h"
+
+//------------------------------------------------------------------------------
+/**
+*/
+int
+main(int argc, const char** argv)
+{
+	Example::ExampleApp app;
+	if (app.Open())
+	{
+		app.Run();
+		app.Close();
+	}
+	app.Exit();	
+
+	return app.ExitCode();
+}
