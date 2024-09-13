626d58d1e7b4bf76cf769172187ba96db45433dd
dkingafg6 <61297626+dkingafg6@users.noreply.github.com>
2024-09-13 23:29:04 +0200

    up

--- projects/graphic/code/MeshResource.h
+++ projects/graphic/code/MeshResource.h
@@ -38,7 +38,7 @@ void MeshResource::createVBO(float x, float y)
 	float position[] =
 	{
 		-x, y, // p1
-		0.0f, 0.0f, 0.0f, 0.0f, // color red green blue alfa (alfa == trancparenci)
+		0.0f, 0.0f, 0.0f, 0.0f, // color red green blue alfa (alfa == transparency)
 		x, y, // p2
 		0.0f, 0.0f, 0.0f, 0.0f,// color RGBA
 		x,-y, // p3
@@ -50,7 +50,7 @@ void MeshResource::createVBO(float x, float y)
 	// generate buffer for binding vertex
 
 	glGenBuffers(1, &vertexBuffer); 
-	// bind the openGL array data with vertextbuffer. 
+	// bind the openGL array data with vertex buffer. 
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer); 
 	// take the information from what type, what size and what position and draw. 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
@@ -60,7 +60,7 @@ void MeshResource::createVBO(float x, float y)
 // IBO = index buffer object
 void MeshResource::createIBO()
 {
-	// coneccting the points to gather. 
+	// connecting the points to gather. 
 	int indexes[] =
 	{
 		0, 1, 2,
@@ -69,13 +69,24 @@ void MeshResource::createIBO()
 	};
 	// generate buffer for binding indexes. 
 	glGenBuffers(1, &indexBuffer);
-	// bind the openGL element array data with indexbuffer. 
+	// bind the openGL element array data with index buffer. 
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 	// take the information from what type, what size and what indexes and draw. 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);  // position
 	glEnableVertexAttribArray(1);  // color 
+
+	glEnableVertexAttribArray(2); // uv
+
+	/*glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 9, NULL);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 9, (GLvoid*)(sizeof(float32) * 3));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 9, (GLvoid*)(sizeof(float32) * 7));*/
+
+	//buffersCreated = true
+		
+
+	
 	
 
 
